7326665c46c1ccbfbac7952f044d10be9c192c8a
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-03-25 16:08:07 -0500

    WKR stat nerf and addtion
    Lowered EVA from 1210 to 1000 and added MEVA and MDEF

--- scripts/zones/Escha_RuAun/mobs/Kouryu.lua
+++ scripts/zones/Escha_RuAun/mobs/Kouryu.lua
@@ -7,6 +7,19 @@ local ID = zones[xi.zone.ESCHA_RUAUN]
 ---@type TMobEntity
 local entity = {}
 
+entity.onMobSpawn = function(mob)
+    -- Create a short overlap before despawning Kirin
+    mob:timer(1500, function(m)
+        local kirinId = m:getLocalVar('KirinID')
+        if kirinId ~= 0 then
+            local kirin = GetMobByID(kirinId)
+            if kirin and kirin:isSpawned() then
+                DespawnMob(kirinId)
+            end
+        end
+    end)
+end
+
 entity.onMobDeath = function(mob, player, optParams)
 
 end
