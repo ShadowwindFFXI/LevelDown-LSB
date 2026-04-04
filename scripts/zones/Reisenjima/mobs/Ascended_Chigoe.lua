8a4474c541a694793e64b37b99162d89267be1cc
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-04-04 15:47:24 -0500

    Update Ascended_Chigoe.lua
    Makes mob targetable

--- scripts/zones/Reisenjima/mobs/Ascended_Chigoe.lua
+++ scripts/zones/Reisenjima/mobs/Ascended_Chigoe.lua
@@ -12,6 +12,24 @@ for _, phId in pairs(ID.mob.ARBOREAL_CHIGOE) do
     entity.phList[phId] = ID.mob.ASCENDED_CHIGOE
 end
 
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.ALWAYS_AGGRO, 1)
+    mob:setUntargetable(true)
+    mob:hideName(true)
+end
+
+entity.onMobEngage = function(mob, target)
+    mob:hideName(false)
+    mob:setUntargetable(false)
+    mob:setAnimationSub(2)
+end
+
+entity.onMobDisengage = function(mob)
+    mob:hideName(true)
+    mob:setUntargetable(true)
+    mob:setAnimationSub(0)
+end
+
 entity.onMobDeath = function(mob, player, optParams)
 
 end
