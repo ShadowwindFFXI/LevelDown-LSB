8c4b6dcb94caaee0a1eea7214766fc10d1c920d7
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-04-04 15:57:22 -0500

    Added Sea and Reisenjima teleports

--- scripts/zones/Konschtat_Highlands/npcs/Dimensional_Portal.lua
+++ scripts/zones/Konschtat_Highlands/npcs/Dimensional_Portal.lua
@@ -9,7 +9,12 @@ local ID = zones[xi.zone.KONSCHTAT_HIGHLANDS]
 local entity = {}
 
 entity.onTrigger = function(player, npc)
-    if player:getCurrentMission(xi.mission.log_id.COP) > xi.mission.id.cop.THE_WARRIORS_PATH then
+    if
+        player:getCurrentMission(xi.mission.log_id.COP) >= xi.mission.id.cop.THE_WARRIORS_PATH and
+        player:getCurrentMission(xi.mission.log_id.ROV) >= xi.mission.id.rov.UNCERTAIN_FUTURES
+    then
+        player:startEvent(926, 2)
+    elseif player:getCurrentMission(xi.mission.log_id.COP) >= xi.mission.id.cop.THE_WARRIORS_PATH then
         player:startEvent(915)
     else
         player:messageSpecial(ID.text.ALREADY_OBTAINED_TELE + 1) -- Telepoint Disappeared
@@ -19,6 +24,10 @@ end
 entity.onEventFinish = function(player, csid, option, npc)
     if csid == 915 and option == 1 then
         player:setPos(-635.599, -2.799, 163.8, 193, 33) -- To AlTaieu (R)
+    elseif csid == 926 and option == 1 then
+        player:setPos(-635.599, -2.799, 163.8, 193, 33) -- To AlTaieu (R)
+    elseif csid == 926 and option == 2 then
+        player:setPos(-500.0230, -19.0838, -487.6860, 190, 291) -- To Reisenjim F-12
     end
 end
 
--- scripts/zones/La_Theine_Plateau/npcs/Dimensional_Portal.lua
+++ scripts/zones/La_Theine_Plateau/npcs/Dimensional_Portal.lua
@@ -8,16 +8,25 @@ local ID = zones[xi.zone.LA_THEINE_PLATEAU]
 local entity = {}
 
 entity.onTrigger = function(player, npc)
-    if player:getCurrentMission(xi.mission.log_id.COP) > xi.mission.id.cop.THE_WARRIORS_PATH then
-        player:startEvent(204)
+    if
+        player:getCurrentMission(xi.mission.log_id.COP) >= xi.mission.id.cop.THE_WARRIORS_PATH and
+        player:getCurrentMission(xi.mission.log_id.ROV) >= xi.mission.id.rov.UNCERTAIN_FUTURES
+    then
+        player:startEvent(926, 2)
+    elseif player:getCurrentMission(xi.mission.log_id.COP) >= xi.mission.id.cop.THE_WARRIORS_PATH then
+        player:startEvent(915)
     else
         player:messageSpecial(ID.text.ALREADY_OBTAINED_TELE + 1) -- Telepoint Disappeared
     end
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    if csid == 204 and option == 1 then
-        player:setPos(25.299, -2.799, 579, 193, 33) -- To AlTaieu (R)
+    if csid == 915 and option == 1 then
+        player:setPos(-635.599, -2.799, 163.8, 193, 33) -- To AlTaieu (R)
+    elseif csid == 926 and option == 1 then
+        player:setPos(-635.599, -2.799, 163.8, 193, 33) -- To AlTaieu (R)
+    elseif csid == 926 and option == 2 then
+        player:setPos(-500.0230, -19.0838, -487.6860, 190, 291) -- To Reisenjim F-12
     end
 end
 
