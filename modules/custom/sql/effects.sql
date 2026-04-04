a38b35e6a7e012f7596b46e24ee6d717e1c6e3dd
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-04-04 15:50:43 -0500

    Update effects.sql
    part of !buff fix

--- modules/custom/sql/effects.sql
+++ modules/custom/sql/effects.sql
@@ -2,3 +2,5 @@ UPDATE status_effects SET flags = 66344 WHERE id = 204; -- no message when effec
 UPDATE status_effects SET flags = 9437440 WHERE id = 476; -- remove on log out, zone, no cancel -- enshphere
 INSERT INTO status_effects VALUES(650, 'paragon', 4194816,0,0,0,0,0,0,0,0); -- lose on job change & no lose message
 UPDATE status_effects SET flags = 4194344 WHERE id = 170; -- wear on job change -- regain
+
+UPDATE `status_effects` SET `flags`=4325376 WHERE `id`=326;
