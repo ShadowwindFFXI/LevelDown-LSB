993078af829d14199491148a70d69d7093695427
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-04-04 15:49:43 -0500

    Update prismatic_breath.lua
    mobFinalAdjustments fix

--- scripts/actions/mobskills/prismatic_breath.lua
+++ scripts/actions/mobskills/prismatic_breath.lua
@@ -12,11 +12,20 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
     return 0
 end
 
-mobskillObject.onMobWeaponSkill = function(target, mob, skill)
+mobskillObject.onMobWeaponSkill = function(target, mob, skill, action)
     xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.INHIBIT_TP, 50, 0, 60)
 
-    local dmgmod = xi.mobskills.mobBreathMove(mob, target, skill, 0.15, 3, xi.element.EARTH, 500)
-    local dmg = xi.mobskills.mobFinalAdjustments(dmgmod, mob, skill, target, xi.attackType.BREATH, xi.damageType.EARTH, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
+    local params = {}
+    params.percentMultipier = 0.15
+    params.fTP = { 3, 3, 3 } -- Assuming 3 is meant to be a fixed fTP for all TP levels
+    params.element = xi.element.EARTH
+    params.damageCap = 500
+    params.attackType = xi.attackType.BREATH
+    params.damageType = xi.damageType.EARTH
+    params.shadowBehavior = xi.mobskills.shadowBehavior.IGNORE_SHADOWS
+
+    local info = xi.mobskills.mobBreathMove(mob, target, skill, action, params)
+    local dmg = xi.mobskills.mobFinalAdjustments(info, mob, skill, target, xi.attackType.BREATH, xi.damageType.EARTH, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, xi.attackType.BREATH, xi.damageType.EARTH, { breakBind = false })
 
     return dmg
