3b7965dff4e02818336d40716a6138ada461881d
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-04-07 11:46:59 -0500

    Update colossal_blow.lua
    damage not defined

--- scripts/actions/mobskills/colossal_blow.lua
+++ scripts/actions/mobskills/colossal_blow.lua
@@ -20,7 +20,7 @@ mobskillObject.onMobWeaponSkill = function(target, mob, skill)
 
     local info =
     {
-        damage
+        damage = damage
     }
 
     local dmg = xi.mobskills.mobFinalAdjustments(info, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.PIERCING, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
