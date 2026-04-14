13eb62eb63b4e65750e22d285455720be71a5bbf
General <75862857+FFXILevelDown@users.noreply.github.com>
2026-04-13 21:51:15 -0600

    removed white space from mob id preventing Kouryu from spawning

--- scripts/zones/Escha_RuAun/IDs.lua
+++ scripts/zones/Escha_RuAun/IDs.lua
@@ -13,10 +13,12 @@ zones[xi.zone.ESCHA_RUAUN] =
         KEYITEM_OBTAINED              = 6394, -- Obtained key item: <keyitem>.
         ITEMS_OBTAINED                = 6400, -- You obtain <number> <item>!
         NOTHING_OUT_OF_ORDINARY       = 6405, -- There is nothing out of the ordinary here.
+        CRUOR_OBTAINED                = 6989, -- Obtained {0} cruor. (Total: {1})<7F31>
         CARRIED_OVER_POINTS           = 7002, -- You have carried over <number> login point[/s].
         LOGIN_CAMPAIGN_UNDERWAY       = 7003, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
         LOGIN_NUMBER                  = 7004, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
         MEMBERS_LEVELS_ARE_RESTRICTED = 7024, -- Your party is unable to participate because certain members' levels are restricted.
+        DOMAIN_POINTS_OBTAINED        = 7778, -- <1F:79>{player:name} obtained {0} Domain Points!
     },
     mob =
     {
@@ -30,7 +32,7 @@ zones[xi.zone.ESCHA_RUAUN] =
         WARDER_OF_MERCY      = GetFirstID('Warder_of_Mercy'),
         WARDER_OF_PRUDENCE   = GetFirstID('Warder_of_Prudence'),
         WARDER_OF_TEMPERANCE = GetFirstID('Warder_of_Temperance'),
-		KIRIN				 = GetFirstID('Kirin'),		
+		KIRIN				 = GetFirstID('Kirin'),
         KOURYU               = GetFirstID('Kouryu'),
         ESCHAN_LIMULE        = GetTableOfIDs('Eschan_Limule'),
         ESCHAN_EUVHI         = GetTableOfIDs('Eschan_Euvhi'),
