6b02e4bc7bb17c00b07a990511be25941b327ee9
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-04-20 09:42:58 -0500

    Update custZoneSettings.sql
    Potential fix for Outer_RaKazner [U1] to show mobskills

--- modules/custom/sql/custZoneSettings.sql
+++ modules/custom/sql/custZoneSettings.sql
@@ -15,3 +15,6 @@ UPDATE zone_settings SET misc = 2712 WHERE zoneid =  298;
 UPDATE `zone_settings` SET `misc`=2716 WHERE `zoneid`=288; -- Escha Zi'Tah
 UPDATE `zone_settings` SET `misc`=2716 WHERE `zoneid`=289; -- Escha Ru'Aun
 UPDATE `zone_settings` SET `misc`=2716 WHERE `zoneid`=291; -- Reisenjima
+
+--Potential fix for Outer_RaKazner [U1] to show mobskills?
+UPDATE `zone_settings` SET `misc`= 6809 WHERE `zoneid`= 275; -- misc was 2714 
