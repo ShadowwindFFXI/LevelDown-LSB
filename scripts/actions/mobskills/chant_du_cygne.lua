c6d44301efab4c93c7dffe5407fa0c654e7f9a5f
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-05-06 15:51:20 -0500

    Create chant_du_cygne.lua
    mobskill from Graves

---
+++ scripts/actions/mobskills/chant_du_cygne.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+-- Chant du Cygne
+--
+-- Description: Delivers a Three-hit attack. Chance of critical varies with TP.
+-- Type: Physical
+-- Utsusemi/Blink absorb: Shadow per hit
+-- Range: Melee
+-----------------------------------
+
+local mobskill_object = {}
+
+mobskill_object.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+
+    local numhits = 3
+    local accmod = 1
+    local dmgmod = 2.25
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.CRIT_VARIES, 0.15, 0.25, 0.4)
+    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
+
+    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
+    return dmg
+end
+
+return mobskill_object
